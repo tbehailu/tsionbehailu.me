0f06bcc797fc12576bb81d64e5ed91f5b1591ecf
tbehailu <tsionbehailu@gmail.com>
2014-09-10 15:38:01 -0700

    minor changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,20 +2,20 @@ class ArticlesController < ApplicationController
 	def new
 	end
 
-def create
-  @article = Article.new(params[:article])
- 
-  @article.save
-  redirect_to @article
-end
+  def create
+    @article = Article.new(params[:article])
+   
+    @article.save
+    redirect_to @article
+  end
 
-def index
-  @articles = Article.all
-end
+  def index
+    @articles = Article.all
+  end
 
-def show
-  @article = Article.find(params[:id])
-end
+  def show
+    @article = Article.find(params[:id])
+  end
 
 private
   def article_params
